985ce4373aef2a205dac51051eef062be0e2da38
Giancarlo Leandro <giancarlosleandro1@gmail.com>
2017-05-18 18:23:31 -0500

    Paso01

---
+++ ConsoleApp2/ConsoleApp2/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Empleados
+    {
+        public string Nombre;
+        public decimal SueldoDiario;
+        public int Edad;
+
+        public Empleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.0m;
+            Edad = 0;
+        }
+
+        public decimal CalculaSalario(int dias)
+        {
+            return SueldoDiario * dias;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleados empleado = new Empleados();
+            empleado.Edad = 20;
+            empleado.Nombre = "Giancarlo Leandro";
+            empleado.SueldoDiario = 15.5m;
+            decimal total = empleado.CalculaSalario(2);
+            Console.WriteLine("El Salario Mensual del empleado " + empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
